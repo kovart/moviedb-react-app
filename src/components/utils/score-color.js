e844698fc6bb35b8e7f76abe98481219f3eee910
Artem Kovalchuk <kovart.dev@gmail.com>
2020-01-06 12:33:47 +0200

    chore(ui): change score colors

--- src/components/utils/score-color.js
+++ src/components/utils/score-color.js
@@ -1,8 +1,8 @@
 export const VoteColors = {
     UNKNOWN: '#999',
     BAD: '#d63737',
-    OK: '#de8622',
-    GOOD: '#b6d645',
+    OK: '#a7a7a7',
+    GOOD: '#5fbb67',
     EXCELLENT: '#3bb33b'
 }
 
@@ -10,8 +10,8 @@ export function getScoreColor(score) {
     let color = VoteColors.EXCELLENT
     if(!score || score === 0) color = VoteColors.UNKNOWN
     else if(score < 5.5) color = VoteColors.BAD
-    else if(score < 7) color = VoteColors.OK
-    else if(score < 8) color = VoteColors.GOOD
+    else if(score < 6.5) color = VoteColors.OK
+    else if(score < 7.25) color = VoteColors.GOOD
 
     return color
 }
